6792cd9a174cfd2d2e6c1f37d3762c1e5809f65a
liy <liy@sinmn.com>
2020-10-08 16:16:53 +0800

    Spring boot 整合 redis

---
+++ src/main/java/org/example/IndexController.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.redisson.Redisson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    private Redisson redisson;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/deduct_stock")
+    public String deductStock() {
+
+        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+        if(stock > 0) {
+            int realStock = stock - 1;
+            stringRedisTemplate.opsForValue().set("stock",realStock + "");
+            System.out.println("扣减成功，剩余库存：" + realStock);
+        } else {
+            System.out.println("扣减失败，库存不足");
+        }
+
+        return "end";
+    }
+
+
+}
